cf42a83ea2d9f05565ef2fd1dccd49a7184ebc88
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-07-05 07:56:08 +0300

    feat: create ErrorPage for handle errors

--- src/pages/GamesPage.tsx
+++
@@ -1,3 +0,0 @@
-export const GamesPage = () => {
-  return <div>Games</div>;
-};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
-import { GamesPage } from '../pages/GamesPage';
 import GameDetailPage from '../pages/GameDetailPage';
 import Layout from '../pages/Layout';
+import ErrorPage from '../pages/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
