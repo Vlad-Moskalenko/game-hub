d67a3e6821c0c013534b58456310edb6872de3ee
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-07-06 08:10:42 +0300

    fix: fix title icon and card link

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,19 +12,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          <NavLink to={`games/${game.slug}`}>{game.name}</NavLink>
-          <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
+    <NavLink to={`games/${game.slug}`}>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </NavLink>
   );
 };
 
